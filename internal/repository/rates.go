d3cc71e13f0d3182c9b95ec10118884166ea254a
Black-tag <unnianandunni007@gmail.com>
2025-08-15 02:57:26 +0000

    added 3 external api calling functions for 3 different endpoints

--- internal/repository/rates.go
+++ internal/repository/rates.go
@@ -13,23 +13,18 @@ import (
 
 
 
-func FetchRates(from, to, date string ) (io.ReadCloser, error) {
+func FetchRates() (io.ReadCloser, error) {
 
 	// API_KEY := "0a40d8593652f00fe31321015eb90267"
 	logger := slog.Default().With(
 		"handler", "FetchRates",
 	)
-	base := strings.ToUpper(from)
-	symbol := strings.ToUpper(to)
-	logger =logger.With("base", base)
-	logger =logger.With("to", symbol)
-	logger =logger.With("date", date)
-	var url string 
-	if date == "" {
-		url = fmt.Sprintf("https://api.exchangerate.host/live?access_key=0a40d8593652f00fe31321015eb90267")
-	} else {
-		url = fmt.Sprintf("https://api.exchangerate.host/historical?access_key=0a40d8593652f00fe31321015eb90267&date=%s", date)
-	}
+	
+	
+	 
+	
+	url := "https://api.exchangerate.host/live?access_key=0a40d8593652f00fe31321015eb90267"
+	
 	logger = logger.With("url", url)
 	logger.Info("making request to the rate excahnge api")
 
@@ -43,6 +38,60 @@ func FetchRates(from, to, date string ) (io.ReadCloser, error) {
 	}
 	
 	logger.Info("successful request")
-	logger = logger.With("response", resp.Body)
+	
+	return resp.Body, nil
+}
+
+
+func FetchHistoricalData(date string) (io.ReadCloser, error) {
+	logger := slog.Default().With(
+		"function", "FetchHistoricalData",
+	)
+	logger =logger.With("date", date)
+	url := fmt.Sprintf("https://api.exchangerate.host/historical?access_key=0a40d8593652f00fe31321015eb90267&date=%s", date)
+	logger = logger.With("url", url)
+	logger.Info("making request to the rate excahnge api")
+
+	httpClient := &http.Client{Timeout: 5* time.Second}
+
+	resp, err := httpClient.Get(url)
+	if err != nil {
+		logger.Error("couldnt fetch response", "error", err)
+		return nil, err
+
+	}
+	
+	logger.Info("successful request")
+	
+	return resp.Body, nil
+
+}
+
+func ConvertAmount(from string, to string, amount int) (io.ReadCloser, error) {
+	logger := slog.Default().With(
+		"function", "FetchHistoricalData",
+	)
+	
+	base := strings.ToUpper(from)
+	symbol := strings.ToUpper(to)
+	logger =logger.With("base", base)
+	logger =logger.With("to", symbol)
+
+	
+	url := fmt.Sprintf("https://api.exchangerate.host/convert?access_key=0a40d8593652f00fe31321015eb90267&from=%s&to=%s&amount=%d", from, to, amount)
+	logger = logger.With("url", url)
+	logger.Info("making request to the rate excahnge api")
+
+	httpClient := &http.Client{Timeout: 5* time.Second}
+
+	resp, err := httpClient.Get(url)
+	if err != nil {
+		logger.Error("couldnt fetch response", "error", err)
+		return nil, err
+
+	}
+	
+	logger.Info("successful request")
+	
 	return resp.Body, nil
 }
