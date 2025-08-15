0f389514b0fe627d91da799c45bf38dc7222ee2b
Black-tag <unnianandunni007@gmail.com>
2025-08-15 02:58:24 +0000

    added added api response model for converting int struct

--- internal/model/model.go
+++ internal/model/model.go
@@ -5,12 +5,12 @@ package model
 
 
 
-type Rateresponse struct {
+type RateResponse struct {
 	Success   bool               `json:"success"`
 	Terms     string             `json:"terms"`
 	Privacy   string             `json:"privacy"`
 	Timestamp int64              `json:"timestamp"`
 	Source    string             `json:"source"`
-	Qoutes    map[string]float64 `json:"qoutes"`
+	Quotes    map[string]float64 `json:"quotes"`
 
 }
