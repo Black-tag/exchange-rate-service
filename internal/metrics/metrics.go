ac6f87e86f4eadd8967499544914098d49505b34
Black-tag <unnianandunni007@gmail.com>
2025-08-16 07:08:55 +0000

    added metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -13,9 +13,14 @@ var (
 		Name: "total_api_requests",
 		Help: "Sum of total requests",
 	},[]string{"endpoint", "method", "status"})
+
+	TotalActicverequest = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "total_active_requests",
+		Help: "current_request",
+	})
 )
 
 
 func Init() {
-	prometheus.MustRegister(TotalApiRequests)
+	prometheus.MustRegister(TotalApiRequests, TotalActicverequest)
 }
